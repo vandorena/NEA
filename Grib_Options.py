0e2f9f26badac4b5432d53e2c0273375d8e25636
vandorena <alex.vandoren@abingdon.org.uk>
2024-06-12 10:23:39 +0100

    Worked on accessor methods to change the gribs delivered by the APIs

--- Grib_Options.py
+++ Grib_Options.py
@@ -15,3 +15,40 @@ BASECLIENT_DATA = Client(
     preserve_request_order=False,
     infer_stream_keyword=True,
 )
+
+class Grib_Modifiers:
+    """Parent Class for respective API modifiers, dealing with the NOAA and ECMWF Models, """
+
+    def __init__(self) -> None:
+        self._current_client = None
+        self._current_address = None
+    
+
+class ECMWF_API(Grib_Modifiers):
+    """ECMWF API grib options, Default Client is Physics driven - IFS, default server is ecmwf, with options for azure"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._current_client = BASECLIENT_PHYSICS
+
+
+    def _change_client(self):
+        """Accessor Method to Change the Current Client, no parameters"""
+        if self._check_client == "DATA":
+            self._current_client = BASECLIENT_PHYSICS
+        else:
+            self._current_client = BASECLIENT_DATA
+    
+    def check_client(self):
+        "Method to see the current client, returns 'DATA' or 'PHYSICS' relating to AIFS and IFS models respectively"
+        if self._current_client == BASECLIENT_DATA:
+            return "DATA"
+        else:
+            return "PHYSICS"
+        
+    def change_source(self):
+        """Method to change the request server, from ecmwf to azure, and vice versa"""
+        if self._current_client.source == "ecmwf":
+            self._current_client.source = "azure"
+        else:
+            self._current_client.source = "ecmwf"
