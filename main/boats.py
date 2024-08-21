67bafea7319e42f253f4cbdb8ccc64ae713e8521
vandorena <alex.vandoren@abingdon.org.uk>
2024-08-21 13:31:20 +0100

    Worked on entitys

---
+++ main/boats.py
@@ -0,0 +1,7 @@
+
+class Boat:
+
+    def __init__(self) -> None:
+        self.data = {
+            "name":"name"
+        }
--- main/path.py
+++ main/path.py
@@ -1,18 +1,33 @@
-
+from boats import Boat
 
 class Path:
 
-    def __init__(self,start_lattitude: float = 0, start_longitude: float = 0,end_latitude: float=0, end_longitude: float = 0) -> None:
+    def __init__(self,start_lattitude: float = 0, start_longitude: float = 0,end_latitude: float=0, end_longitude: float = 0, boat: Boat=Boat) -> None:
         
         self.start_lattitude = start_lattitude
         self.end_lattitude = start_longitude
         self.start_longitude = start_longitude
         self.end_longitude = end_longitude
 
+        self._current_point_lat = start_lattitude
+        self._current_point_lon = start_longitude
+
+        self.current_boat = boat
+        self._started = False
+
         self.path_data = {
             "lat":[],
             "lon":[],
+            "times":[],
+            "speeds":[],
             "windspeed":[],
-            "wind_direction":[]
-            
+            "gust_speed":[],
+            "wind_direction":[],
+            "wave_height":[],
+            "wave_direction":[],
+            "air_pressure":[]
         }
+
+    def get_previous_point(self):
+        if self._started == False:
+            return 
