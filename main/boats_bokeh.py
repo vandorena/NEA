85faaab167a4906a03298c1188cfc5d29633dfe7
vandorena <alex.vandoren@abingdon.org.uk>
2024-08-22 09:17:43 +0100

    Worked on the boats_bokeh file

--- main/boats_bokeh.py
+++ main/boats_bokeh.py
@@ -1,3 +1,19 @@
+from boats import Boat
+
+
+CURRENT_BOATS = {"boat_list":[]}
+
+def find_boats():
+    with open("..\Boats\Boat_saves.txt","r") as file:
+        boats = file.readlines()
+    for i in range(0,len(boats)):
+        line_content = boats[i].split()
+        boat = Boat(line_content[0])
+        CURRENT_BOATS["boat_list"].append(line_content[0])
+        boat.add_polar(line_content[1])
+        CURRENT_BOATS[line_content[0]] = boat
+        boat = None
+        
 
 def boats(doc):
-    pass
+    find_boats()
