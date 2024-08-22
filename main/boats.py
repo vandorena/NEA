8414a76b3c7d305576bd912e83cfa205a6e633a2
vandorena <alex.vandoren@abingdon.org.uk>
2024-08-22 13:09:41 +0100

    worked on my binary search stuffs

--- main/boats.py
+++ main/boats.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class PolarFileError(Exception):
     """Exception Raised when the Polar File is non homogenous"""
 class Boat:
@@ -46,10 +48,39 @@ class Boat:
         for i in range(0,len(list)):
             new_list.append(int(list[i]))
         return new_list
-
-    def find_polar_speed(self,windspeed,heading):
-        reference_speeds = self._list_to_int(self.data["wind_list"])
-        reference_headings = self._list_to_int(self.data["heading_list"])
-        current_list_length = len(reference_speeds)
+    
+    def _binary_list_class_search(self,input_list: list, search_term: int):
+        list_length = len(input_list)
+        current_index = list_length//2
         found = False
+        current_comparision = 0
+        max_comparison = np.log2(list_length)
+        count = 1
+        found_index = -1
         while not found:
+            if current_comparision <= max_comparison:
+                if input_list[current_index] == search_term:
+                    found_index = current_index
+                    found = True
+                else:
+                    count +=1
+                    if input_list[current_index] < search_term:
+                        current_index = current_index + list_length//(2**count)
+                    else:
+                        current_index = current_index - list_length//(2**count)
+            else:
+                for i in range(0,list_length):
+                    if input_list[i-1] < search_term and input_list[i] > search_term:
+                        found_index = i
+                        found = True
+        return found_index
+                
+    def find_polar_speed(self,windspeed,heading):
+        reference_windspeeds = self._list_to_int(self.data["wind_list"])
+        reference_headings = self._list_to_int(self.data["heading_list"])
+        speed_index = self._binary_list_class_search(reference_windspeeds,windspeed)
+        heading_index = self._binary_list_class_search(reference_headings,heading)
+        boatspeed = self.data[self.data["wind_list"][speed_index]][heading_index]
+        return boatspeed
+        
+        
