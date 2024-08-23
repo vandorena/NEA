387e9d16363a322307adf4f8344354737577f4b5
vandorena <alex.vandoren@abingdon.org.uk>
2024-08-23 15:13:33 +0100

    worked on grib modifiers

--- main/Grib_Options.py
+++ main/Grib_Options.py
@@ -1,5 +1,6 @@
 from ecmwf.opendata import Client
 import datetime
+import os
 
 BASECLIENT_PHYSICS = Client(
     source="ecmwf",
@@ -25,11 +26,13 @@ class Grib_Modifiers:
         self._current_address = None
         self._current_folder = None
         self._update_folder_path()
+        self._request = None
 
     def _update_folder_path(self):
         "Updates Folder Path to current time"
         current_datetime = datetime.datetime.now()
-        self._current_folder = f"..\gribs\{current_datetime.date}\{current_datetime.time.hour}-{current_datetime.time.minute}-{current_datetime.time.second}"
+        os.mkdir(rf"..\gribs\{current_datetime.date()}")
+        self._current_folder = f"..\gribs\{current_datetime.date()}\{current_datetime.hour}-{current_datetime.minute}-{current_datetime.second}.grib2"
         self._current_folder = rf"{self._current_folder}"
     
 
@@ -39,6 +42,10 @@ class ECMWF_API(Grib_Modifiers):
     def __init__(self) -> None:
         super().__init__()
         self._current_client = BASECLIENT_PHYSICS
+        self._request = {
+            "time": self._get_time(),
+            "step": 48,
+                         }
 
 
     def _change_client(self):
@@ -54,7 +61,18 @@ class ECMWF_API(Grib_Modifiers):
             return "DATA"
         else:
             return "PHYSICS"
-        
+
+    def _get_time(self) -> int:
+        current_datetime = datetime.datetime.now()
+        if current_datetime.hour >= 0 and current_datetime.hour < 6:
+            return "0"
+        elif current_datetime.hour >= 6 and current_datetime.hour < 12:
+            return "6"
+        elif current_datetime.hour >= 12 and current_datetime.hour <18:
+            return "12"
+        else:
+            return "18"
+
     def change_source(self):
         """Method to change the request server, from ecmwf to azure, and vice versa"""
         if self._current_client.source == "ecmwf":
@@ -66,7 +84,8 @@ if __name__ == "__main__":
     client = Client()
 
     current_datetime = datetime.datetime.now()
-    current_folder = f"..\gribs\{current_datetime.date}\{current_datetime.time.hour}-{current_datetime.time.minute}-{current_datetime.time.second}"
+    os.mkdir(rf"..\gribs\{current_datetime.date()}")
+    current_folder = f"..\gribs\{current_datetime.date()}\{current_datetime.hour}-{current_datetime.minute}-{current_datetime.second}.grib2"
     current_folder = rf"{current_folder}"
 
     client.retrieve(
