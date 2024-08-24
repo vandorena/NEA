fd2b8f0adb3a5f91f5d274111a786c8bac7d7293
vandorena <alex.vandoren@abingdon.org.uk>
2024-08-24 16:44:48 +0100

    Worked on file organisation of weather_bokeh.py

--- main/weather_bokeh.py
+++ main/weather_bokeh.py
@@ -1,9 +1,6 @@
 import os,datetime
-CURRENT_TIME = datetime.datetime.now()
 
-CURRENT_GRIBS = {
-    "grib_list":[]
-}
+CURRENT_TIME = datetime.datetime.now()
 
 CURRENT_SUBFOLDERS ={
     "subfolder_list":[]
@@ -12,9 +9,13 @@ CURRENT_SUBFOLDERS ={
 def find_gribs():
     grib_folder_list = os.listdir("..//gribs")
     for i in range(0,len(grib_folder_list)):
-        print(grib_folder_list[i][:5])
-        print(grib_folder_list[i])
         if grib_folder_list[i][:5] == f"{CURRENT_TIME.year}-" and grib_folder_list[i][7] == "-":
-            print("Ya")
+            CURRENT_SUBFOLDERS["subfolder_list"].append(grib_folder_list[i])
+            new_folder_gribs = os.listdir(f"..//gribs//{grib_folder_list[i]}")
+            for i in range(0,len(new_folder_gribs)):
+                if new_folder_gribs[i][-6:] != ".grib2":
+                    new_folder_gribs.remove(new_folder_gribs[i])
+            CURRENT_SUBFOLDERS[grib_folder_list[i]] = new_folder_gribs
 
-find_gribs()
+if __name__ == "__main__":
+    find_gribs()
