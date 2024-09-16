a5289b637c8c312618cccfefd8c277c0144310ad
vandorena <alex.vandoren@abingdon.org.uk>
2024-09-16 11:01:41 -0400

    more settings

--- main/settings_bokeh.py
+++ main/settings_bokeh.py
@@ -1,7 +1,22 @@
 import bokeh
 from bokeh.io import curdoc
-from bokeh.models import Button, Div, Dropdown, Paragraph, DatetimeRangeSlider
-from globals import SETTINGS
+from bokeh.models import Button, Slider
+from bokeh.layouts import row,column
+from globals import SETTINGS,mapheight,mapwidth
+
+def update_width_height(attr,old,new):
+    global mapheight,mapwidth
+    mapheight = map_height_slider.value
+    mapwidth = map_width_slider.value
+    return
 
 def settings(doc):
-    pass
+
+    map_width_slider = Slider(start=100,end=1000,step=10,value=mapwidth,lable="Map Width in Pixels")
+    map_height_slider = Slider(start=100,end=900,step=10,value=mapheight,lable="Map Height in Pixels")
+
+    map_width_slider.on_change("value", update_width_height)
+
+    column1 = column(map_height_slider,map_width_slider)
+    doc.add_root()
+    
