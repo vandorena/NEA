8ff4c20f5778b539a787d71828990ced91f2de94
vandorena <alex.vandoren@abingdon.org.uk>
2024-09-27 10:30:13 -0400

    Finished logic for gribs

--- main/Grib_Options.py
+++ main/Grib_Options.py
@@ -3,9 +3,17 @@ import datetime
 import os
 import globals
 from eccodes import codes_grib_new_from_file, codes_get_values, codes_get, codes_release
+import numpy as np
 
-class Incompatible_Grid_Type(Exception):
-    """"Exception relating to the type of Grid, raised if the type of Grid used by the Grib message is not 'regular_ll' """
+class Bad_Grib(Exception):
+    """Exception for a bad grib"""
+
+class Incompatible_Grid_Type(Bad_Grib):
+    """Exception relating to the type of Grid, raised if the type of Grid used by the Grib message is not 'regular_ll' """
+
+class Incompatible_level_information(Bad_Grib):
+    """Exception for gribs containing messages with multiple levels"""
+
 
 class Grib_Modifiers:
     """Parent Class for respective API modifiers, dealing with the NOAA and ECMWF Models, """
@@ -19,10 +27,8 @@ class Grib_Modifiers:
     def _update_folder_path(self):
         "Updates Folder Path to current time"
         current_datetime = datetime.datetime.now()
-        os.mkdir(rf"..\gribs\{current_datetime.date()}")
-        self._current_folder = f"..\gribs\{current_datetime.date()}\{current_datetime.hour}-{current_datetime.minute}-{current_datetime.second}.grib2"
-        self._current_folder = rf"{self._current_folder}"
-    
+        os.mkdir(os.path.join("gribs",f"{current_datetime.date()}"))
+        self._current_folder = os.path.join("gribs",f"{current_datetime.date()}",f"{current_datetime.hour}-{current_datetime.minute}-{current_datetime.second}.grib2")
 
 class ECMWF_API(Grib_Modifiers):
     """ECMWF API grib options, Default Client is Physics driven - IFS, default server is ecmwf, with options for azure"""
@@ -85,27 +91,38 @@ class GRIB:
 
     def __init__(self, file_name:str) -> None:
         """File_name includes the .grib,.grib2 or .grb extension"""
-        self._path = rf"gribs/{file_name}"
-        self._data = {
-            "latitudes" :[],
-            "longitudes" :[],
-            "times":[],
-            "data":[]
-        }
-        self._data_list = None
-        self._datetime = None
+        self._path = os.path.join("gribs",file_name)
+        self._path_ok = True
+        if not os.path.exists(self._path):
+            self._path_ok = False
+        else:
+            self._data = {
+                "index": [],
+                "short_name_list":[],
+                "latitudes" :[],
+                "longitudes" :[],
+                "times":[],
+                "level_list": [],
+                "time_list": [],
+                "date_list":[],
+            }
+            self._data_list = None
+            self._datetime = None
+            self._read_all()
+            print(self._data)
 
-    def create_distributed_array(self,number_of_points: int,first_point: int,last_point: int)->list:
+
+    def _create_distributed_array(self,number_of_points: int,first_point: int,last_point: int)->list:
         step = (last_point - first_point)/number_of_points
         points = [first_point]
         while points[-1] < last_point:
             new_point = points[-1] + step
-            points.append()
+            points.append(new_point)
             if points[-1] >= last_point:
                 break
         return points
 
-    def create_grid(self):
+    def _create_grid(self):
 
         with open(self._path, "rb") as file:
             current_message = codes_grib_new_from_file(file)
@@ -119,41 +136,87 @@ class GRIB:
                 last_long = codes_get(current_message,"longitudeOfLastGridPointInDegrees")
                 first_lat = codes_get(current_message,"latitudeOfFirstGridPointInDegrees")
                 last_lat = codes_get(current_message,"latitudeOfLastGridPointInDegrees")
-                self._data["latitudes"] = self.create_distributed_array(number_of_latitudes,first_lat,last_lat)
-                self._data["longitudes"] = self.create_distributed_array(number_of_longitudes,first_long,last_long)
+                self._data["latitudes"] = self._create_distributed_array(number_of_latitudes,first_lat,last_lat)
+                self._data["longitudes"] = self._create_distributed_array(number_of_longitudes,first_long,last_long)
             file.close()
 
-    def read_all(self):
-        self.create_grid
+    
+    def _read_all(self):
+        self._create_grid()
         with open(self._path, 'rb') as file:
             big_list = []
             end_file = False
 
             while not end_file:
-
-                current_message = codes_grib_new_from_file(file)
-                
-                if current_message == None:
-                    end_file = True
+                try:
+                    current_message = codes_grib_new_from_file(file)
+                    if current_message == None:
+                        end_file = True
+                        break
+                    values = codes_get_values(current_message)
+                    name = codes_get(current_message,"shortName")
+                    date = codes_get(current_message,"date")
+                    time = codes_get(current_message,"time")
+                    if codes_get(current_message,"bottomLevel") == codes_get(current_message,"topLevel"):
+                        values.append(codes_get(current_message,"bottomLevel"))
+                    else:
+                        raise Incompatible_level_information("Message has multiple Levels")
+                    values.append(time)
+                    values.append(date)
+                    values.append(name)
+                    big_list.append(values)
+                except Exception:
+                    file.close()
+                    raise Bad_Grib("Grib is corrupt")
+            
             file.close()
+        self._data_digest(big_list)
+        
+
+    def _reshape_array(self,values: list):
+        nj = len(self._data["latitudes"])
+        ni = len(self._data["longitudes"])
+        return np.reshape(values,(nj,ni))
+        
+    def _digest_per_values(self,values:list):
+        short_name = values.pop()
+        date = values.pop()
+        time = values.pop()
+        level = values.pop()
+        if short_name not in self._data["short_name_list"]:
+            self._data["short_name_list"].append(short_name)
+        if level not in self._data["level_list"]:
+            self._data["level_list"].append(level)
+        if time not in self._data["time_list"]:
+            self._data["time_list"].append(time)
+        if date not in self._data["date_list"]:
+            self._data["date_list"].append(date)
+        index = short_name + date + time + level# all type str
+        self._data["index"].append(index)
+        self._data[index] = self._reshape_array(values)
+        return
 
 
-
-    def _data_digest(self):
-        pass
+    def _data_digest(self,big_list:list):
+        for i in range(0,len(big_list)):
+            self._digest_per_values(big_list[i])
+        print(self._data["short_name_list"])
+        print(self._data["index"])
         
 
 if __name__ == "__main__":
-    client = Client()
+    # client = Client()
 
-    current_datetime = datetime.datetime.now()
-    os.mkdir(rf"..\gribs\{current_datetime.date()}")
-    current_folder = f"..\gribs\{current_datetime.date()}\{current_datetime.hour}-{current_datetime.minute}-{current_datetime.second}.grib2"
-    current_folder = rf"{current_folder}"
+    # current_datetime = datetime.datetime.now()
+    # os.mkdir(rf"..\gribs\{current_datetime.date()}")
+    # current_folder = f"..\gribs\{current_datetime.date()}\{current_datetime.hour}-{current_datetime.minute}-{current_datetime.second}.grib2"
+    # current_folder = rf"{current_folder}"
 
-    client.retrieve(
-        step=240,
-        type="fc",
-        param="msl",
-        target=current_folder,
-    )
+    # client.retrieve(
+    #     step=240,
+    #     type="fc",
+    #     param="msl",
+    #     target=current_folder,
+    # )
+    grib = GRIB(r"thefrib.grib2")
+    grib._read_all()
