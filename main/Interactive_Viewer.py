efa97f5c5c670248eac1617e42e4283f0eec5018
vandorena <alex.vandoren@abingdon.org.uk>
2025-01-12 12:31:38 -0500

    Messing around with web mercator

--- main/Interactive_Viewer.py
+++ main/Interactive_Viewer.py
@@ -72,11 +72,6 @@ def viewer(doc):
     def full_grib_routing(event):
         pass
 
-    def mercator_to_latlon(x, y):
-        r_major = 6378137.000
-        lon = np.degrees(x / r_major)
-        lat = np.degrees(2 * np.arctan(np.exp(y / r_major)) - np.pi / 2)
-        return lat, lon
 
     plot = figure(x_range=(-2000000, 2000000), y_range=(1000000, 7000000),
             x_axis_type="mercator", y_axis_type="mercator", width = 1500, height = 900)
@@ -168,33 +163,47 @@ def viewer(doc):
 
     button_enable_grib.on_event("button_click",enable_grib)
 
+    def web_mercator_to_lat_lon(x,y):
+        radius_of_earth_Spherical_Projection = 6378137
+        new_x_lon = np.degrees(x / radius_of_earth_Spherical_Projection)
+        new_y_lat = np.degrees(2 * np.arctan(np.exp(y / radius_of_earth_Spherical_Projection)) - np.pi / 2)
+        return new_y_lat, new_x_lon
+
+    def lat_lon_to_web_mercator(lat,lon):
+        pass
+
     def update_lines():
         pass
 
     def update_div():
         nonlocal start_x,start_y, end_x , end_y, start_x_changed,start_y_changed,end_x_changed,end_y_changed
+        #print(f"start_x_changed: {start_x_changed}, start_y_changed: {start_y_changed}, end_x_changed: {end_x_changed}, end_y_changed: {end_y_changed}")
         if not start_x_changed and not start_y_changed and not end_x_changed and not end_y_changed:
             explainer_div.text = "<h1>Select a start and end point</h1><br>"
-        if start_x_changed and start_y_changed and not end_x_changed and not end_y_changed:
+        elif start_x_changed and start_y_changed and not end_x_changed and not end_y_changed:
+            y_holder_1 , x_holder_1 = web_mercator_to_lat_lon()
             explainer_div.text = (f"<h1> You have not selected an end point</h1><br>"
-                             f"<b>Starting Latitude:</b> {start_y} <br>"
-                             f"<b>Starting Longitude:</b> {start_x} <br>")
-        if start_x_changed and start_y_changed and end_x_changed and end_y_changed:
-            explainer_div.text = (f"<b>Starting Latitude:</b> {start_y} <br>"
-                                  f"<b>Starting Longitude:</b> {start_x} <br>"
-                                  f"<b>End Latitude:</b> {end_y}<br>"
-                                  f"<b>End Longitude;</b> {end_x}<br>")
+                             f"<b>Starting Latitude:</b> {y_holder_1:.5f} <br>"
+                             f"<b>Starting Longitude:</b> {x_holder_1:.5f} <br>")
+        elif start_x_changed and start_y_changed and end_x_changed and end_y_changed:
+            y_holder_1, x_holder_1 = web_mercator_to_lat_lon()
+            y_holder_2, x_holder_2 = web_mercator_to_lat_lon()
+            explainer_div.text = (f"<b>Starting Latitude:</b> {y_holder_1:.5f} <br>"
+                                  f"<b>Starting Longitude:</b> {x_holder_1:.5f} <br>"
+                                  f"<b>End Latitude:</b> {y_holder_2:.5f}<br>"
+                                  f"<b>End Longitude;</b> {x_holder_2:.5f}<br>")
         else:
             print("This shouldn't do this Interactive Viewer.py line 188")
+
     def on_tap(event):
         nonlocal tap_count, start_x,start_y,end_x,end_y,start_x_changed,start_y_changed,end_x_changed,end_y_changed
-        lat,lon = mercator_to_latlon(event.x,event.y)
+        lon,lat = event.x,event.y
         tap_count +=1
         if tap_count == 1:
             pin_point_source.data = {'x':[], 'y':[], 'color':[]}
             start_x = lon
             start_y= lat
-            plot.scatter(x=[lon],y=[lat],size=5,fill_color="red",line_color="yellow",line_width=2)
+            plot.scatter(x=[start_x],y=[start_y],size=10,fill_color="red",line_color="yellow",line_width=1)
             start_x_changed = True
             start_y_changed = True
             update_div()
@@ -203,14 +212,16 @@ def viewer(doc):
             pin_point_source.data= {'x':[],'y':[],'color':[]}
             end_x = lon
             end_y = lat
-            plot.scatter(x=[lon],y=[lat],size=5,fill_color = 'blue',line_color="yellow",line_width=2)
-            update_div()
-            update_lines()
             end_x_changed = True
             end_y_changed = True
+            plot.scatter(x=[end_x],y=[end_y],size=10,fill_color = 'blue',line_color="yellow",line_width=1)
+            update_div()
+            update_lines()
+            
         elif tap_count == 3:
             pin_point_source.data = {'x':[],'y':[],'colors':[]}
             plot.renderers = []
+            plot.add_tile("CartoDB Positron", retina=True)
             tap_count = 0
             start_x = 0
             end_x = 0
@@ -223,16 +234,16 @@ def viewer(doc):
             update_div()
             update_lines()
 
-    
+    plot.on_event(Tap,on_tap)
 
     def update_root(enable_grib: bool):
         doc.clear()
         if not enable_grib:
             bottom_row = row(button_home, button_enable_grib)
-            layout1 = column(current_boat, button_Start_Routing_Full,button_Start_Routing_GCR,boat_dropdown,bottom_row)
+            layout1 = column(explainer_div,current_boat, button_Start_Routing_Full,button_Start_Routing_GCR,boat_dropdown,bottom_row)
         else:
             bottom_row = row(button_home, button_disable_grib)
-            layout1 = column(current_boat, button_Start_Routing_Full_GRIB, button_Start_Routing_GCR_Grib, boat_dropdown,grib_file_input,bottom_row)
+            layout1 = column(explainer_div,current_boat, button_Start_Routing_Full_GRIB, button_Start_Routing_GCR_Grib, boat_dropdown,grib_file_input,bottom_row)
 
         
         layout_main = row(plot,layout1)
